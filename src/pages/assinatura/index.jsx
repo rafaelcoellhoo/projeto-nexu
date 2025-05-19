dad915b1e773472bb7a0fd8c5ea8afbc590303a6
Andre9658 <andrelimanegocio@gmail.com>
2025-05-19 11:08:33 -0300

    assinatura

--- src/pages/assinatura/index.jsx
+++ src/pages/assinatura/index.jsx
@@ -1,26 +1,35 @@
-export default function assinatura () {
-    return (
-        
-            <div className="w-[60%] h-full flex items-center p-[40px] flex-col">
-                <h1 className="text-[45px] w-[60%] h-[150px] font-bold ">
-                    Preencha os Campos para concluir o pagamento
-                </h1>
-                <div className="w-[65%] h-auto min-h-[200px] flex flex-col gap-4 mt-2">
-                    <div className="w-full flex flex-col">
-                        <label htmlFor="nome" className="text-[20px] rounded-md bg-amber-300">
-                            Nome do Cartão
-                        </label>
-                        <input type="text" 
-                        className="w-full h-[40px] rounded-md bg-[#D9D9D9]"/>
-                    </div>
-                    <div className="w-full flex flex-col">
-                        <label htmlFor="numero" className="text-[20px] rounded-md bg-amber-300">
-                            Número do Cartão
-                        </label>
-                        <input type="text" 
-                        className="w-full h-[40px] rounded-md bg-[#D9D9D9]"/>
-                    </div>
-                    <div className="flex">
+import { CiCreditCard2 } from "react-icons/ci";
+import { GoNumber } from "react-icons/go";
+
+
+export default function assinatura() {
+  return (
+
+    <div className="flex items-center justify-center w-full h-full ">
+      <div className="w-[50%] h-full flex items-center p-[40px] flex-col border-2 bg-zinc-200 border-amber-300">
+        <h1 className="text-[35px] h-full font-bold bg-amber-300 ">
+          Preencha os Campos para concluir o pagamento
+        </h1>
+        <div className="w-full h-full flex flex-col gap-4 mt-2">
+          <div className="w-full h-full flex flex-col">
+            <label htmlFor="nome" className="text-[20px] rounded-md bg-amber-300">
+
+              Nome do Cartão
+            </label>
+            <input type="text"
+              className="w-full h-[40px] rounded-md bg-[#D9D9D9]"
+            /> 
+          </div>
+          <CiCreditCard2 size={50}  />
+          <div className="w-full flex flex-col">
+            <label htmlFor="numero" className="text-[20px] rounded-md bg-amber-300">
+              Número do Cartão
+            </label>
+            <input type="text"
+              className="w-full h-[40px] rounded-md bg-[#D9D9D9]" />
+            <GoNumber />
+          </div>
+          <div className="flex">
             <div className="w-[70%] flex flex-col">
               <label htmlFor="" className="text-[20px] rounded-md bg-amber-300">
                 Data de expiração
@@ -48,7 +57,7 @@ export default function assinatura () {
               />
             </div>
           </div>
-            <div className="w-full flex flex-col">
+          <div className="w-full flex flex-col">
             <label htmlFor="" className="text-[20px] rounded-md bg-amber-300">
               Senha do cartão
             </label>
@@ -57,8 +66,9 @@ export default function assinatura () {
               className="w-full h-[40px] rounded-md pl-2 bg-[#D9D9D9]"
             />
           </div>
-          </div>
         </div>
-        
-    )
+      </div>
+    </div>
+
+  )
 }
