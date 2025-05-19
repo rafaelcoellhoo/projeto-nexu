0941f03d96bcab10488d9d50fafb2d68a92ab610
Mikelemag <mikelepinto2017@gmail.com>
2025-05-19 11:15:10 -0300

    login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,46 +1,82 @@
-import sports from "/public/sports.svg"
+
 import { ToastContainer } from "react-toastify"
+import { FcGoogle } from "react-icons/fc"
+import { useState } from "react"
 
 
-export default function Login(){
-    return(
-        <div className="w-full h-screen flex border rounded-md items-center justify-center bg-[#023d0f]">
+export default function Login() {
+  const [user, setUser] = useState("")
+  const [pass, setPass] = useState("")
+  
 
-            <div className="w-[30%] h-full flex items-center justify-center">
-          <div className="w-[70%] h-[350px] bg-[#111111] rounded-md">
+  function entrar(){
+    window.location.href = "/assinatura"
+
+     
+  }
+
+
+
+
+
+  return (
+    <div className="w-full h-screen flex border rounded-md items-center justify-center bg-[#151c16]">
+      <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      theme="colored"
+      
+      />
+   
+    
+
+      <div className="w-[30%] h-full flex items-center justify-center">
+        <div className="w-[70%] h-[450px] bg-[#141414] rounded-md">
           <div className="w-full h-[15%] flex items-center justify-center">
-            <h1 className="text-[30px] text-[#fffcfc] font-stretch-extra-condensed">LOGIN</h1>
+            <h1 className="text-[30px] text-[#74c57d] font-stretch-extra-condensed">Bem vindo(a) de volta!</h1>
           </div>
           <div className="w-full h-[60%] p-[20px]">
-            <label htmlFor="user" className="text-white">Usuário</label>
-            <input 
-             
-               type="text" 
-               id="user" 
-               className="w-full rounded-md p-[10px] bg-[#d6c7c7] text-white mb-4"
+            <label htmlFor="user" className="text-[#74c57d]">Usuário</label>
+            <input
+
+              type="text"
+              id="user"
+              className="w-full rounded-md p-[10px] bg-[#d6c7c7] text-[#0e0e0e] mb-4"
             />
-            <label htmlFor="pass" className="text-white">Senha</label>
-            <input 
-                
-              
+            <label htmlFor="pass" className="text-[#74c57d]">Senha</label>
+            <input
+
+
               type="password"
-              id="pass" 
-            className="w-full rounded-md p-[10px] bg-[#d6c7c7] text-white"
+              id="pass"
+              className="w-full rounded-md p-[10px] bg-[#d6c7c7] text-[#0e0e0e]"
             />
-            <div className="w-full flex items-center justify-center text-white pt-1">
+            <button
+              className="w-full h-[50px] rounded-lg mt-6 bg-[#87e099] text-[20px]
+             font-semibold text-[#201b2c] cursor-pointer"
+             onClick={entrar}
+             >ENTRAR
+
+            </button>
+            <div className="w-full flex items-center justify-center text-[#74c57d] pt-2">
               <a href="">Esqueceu sua Senha?</a>
             </div>
           </div>
-          
+
           <div className="w-full h-[25%] p-[20px] flex items-center">
-            <button 
-            className="w-full h-[50px] rounded-lg bg-[#fcf9fc] text-[18px] font-bold text-[#201b2c] ">LOGIN</button>
-          </div>
-          </div>
+
+            <button
+              className="w-full h-[50px] rounded-lg flex flex-col items-center pt-[2px] bg-[#87e099] text-[15px] 
+           font-semibold text-[#201b2c] cursor-pointer"
+            >Entrar com Google
+              <FcGoogle size={50}/>
+            </button>
           </div>
 
+        </div>
       </div>
 
+    </div>
 
 
 
@@ -49,6 +85,7 @@ export default function Login(){
 
 
 
-        
-    )
+
+
+  )
 }
