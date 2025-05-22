38a6191610d1b2e51d7401af37f663f3712685d8
unknown <franciscoandrade0487@digitalcollege.internal>
2025-05-22 11:52:29 -0300

    assinatura-quin

--- src/pages/assinatura/index.jsx
+++ src/pages/assinatura/index.jsx
@@ -1,72 +1,78 @@
-
+import { useState } from "react"
+import { BsQrCode } from "react-icons/bs";
 
 
 
 
 export default function assinatura() {
+  const [pix, setPix] = useState(false)
+  
   return (
-    <div>
-      <h1 className="text-2x1 text-center bg-[#aadfa8] p-4 font-bold">Selecione a Forma de Pagamento</h1>
-      <div className="w-full bg-[#d9d9d9] grid grid-cols-1">
-        <div className="flex items-center justify-center w-full h-full ">
-          <div className="w-[35%] h-full rounded-lg flex items-center p-[40px] flex-col border-2 bg-zinc-200 border-[#DAA520]">
-            <h1 className="text-[25px] h-full font-bold rounded text-[#DAA520] ">
-              *Cartão*
-            </h1>
-            <h1 className="text-[25px] h-full font-bold rounded text-[#BF2034]">
-              *Pix*
-            </h1>
-            <div className="w-full h-full flex flex-col gap-4 mt-2">
-              <div className="w-full h-full flex flex-col">
-                <label htmlFor="nome" className="text-[20px] rounded-md text-black">
-                  Nome do Cartão
+
+
+    <div className="w-full h-screen bg-[#d9d9d9] grid grid-cols-1">
+      <div className="flex items-center justify-center w-full h-full ">
+        <div className="w-[35%]  rounded-lg flex items-center  p-[40px] flex-col border-2 bg-zinc-200 border-[#DAA520]">
+          <div className="flex gap-3">
+          <button onClick={() => setPix(false)} className="bg-[#daa520] hover:bg-[#bf2034] text-white font-bold py-2 px-4 rounded cursor-pointer">
+            Cartão
+          </button>
+          <button onClick={() => setPix(true)} className="bg-[#daa520] hover:bg-[#bf2034] text-white font-bold py-2 px-4 rounded cursor-pointer">
+            Pix
+          </button>
+          </div>
+          {pix ? <div className="mt-8 flex flex-col items-center justify-center"><BsQrCode size={230} color="black" />bdhhavbhBSHJGHDGDSBDHHGHDDGSHGBbhjBAHB
+          jdshvbhdbhvdbhadsjrbvhbwhfbhwdhbadshlbvahhebL
+          </div> : <div className="w-full h-full flex flex-col gap-4 mt-2">
+            <div className="w-full h-full flex flex-col">
+              <label htmlFor="nome" className="text-[20px] rounded-md text-black">
+                Nome do Cartão
+              </label>
+              <input type="text"
+                className="w-full h-[40px] rounded-md bg-[#D9D9D9]"
+              />
+            </div>
+            <div className="w-full flex flex-col">
+              <label htmlFor="numero" className="text-[20px] rounded-md text-black">
+                Número do Cartão
+              </label>
+              <input type="number"
+                className="w-full h-[40px] rounded-md bg-[#D9D9D9]" />
+            </div>
+            <div className="flex">
+              <div className="w-[70%] flex flex-col">
+                <label htmlFor="" className="text-[20px] rounded-md  text-black">
+                  Data de expiração
                 </label>
-                <input type="text"
-                  className="w-full h-[40px] rounded-md bg-[#D9D9D9]"
-                />
-              </div>
-              <div className="w-full flex flex-col">
-                <label htmlFor="numero" className="text-[20px] rounded-md text-black">
-                  Número do Cartão
-                </label>
-                <input type="number"
-                  className="w-full h-[40px] rounded-md bg-[#D9D9D9]" />
-              </div>
-              <div className="flex">
-                <div className="w-[70%] flex flex-col">
-                  <label htmlFor="" className="text-[20px] rounded-md  text-black">
-                    Data de expiração
-                  </label>
-                  <div className="w-full flex gap-2">
-                    <input
-                      type="number"
-                      placeholder="MM"
-                      className="w-[50%] pl-2 h-[40px] rounded-md bg-[#D9D9D9]"
-                    />
-                    <input
-                      type="number"
-                      placeholder="AA"
-                      className="w-[50%] pl-2 h-[40px] rounded-md bg-[#D9D9D9]"
-                    />
-                  </div>
-                </div>
-                <div className="w-[30%] pl-2 flex flex-col">
-                  <label htmlFor="" className="text-[20px] rounded-md text-black">
-                    CVV
-                  </label>
+                <div className="w-full flex gap-2">
                   <input
                     type="number"
-                    className="w-full h-[40px] rounded-md bg-[#D9D9D9] pl-2"
+                    placeholder="MM"
+                    className="w-[50%] pl-2 h-[40px] rounded-md bg-[#D9D9D9]"
+                  />
+                  <input
+                    type="number"
+                    placeholder="AA"
+                    className="w-[50%] pl-2 h-[40px] rounded-md bg-[#D9D9D9]"
                   />
                 </div>
               </div>
-              <button className="w-full h-[50px] rounded-lg bg-amber-100 text-[20px] font-semibold text-[#201b2c] cursor-pointer">
-                Finalizar Compra
-              </button>
+              <div className="w-[30%] pl-2 flex flex-col">
+                <label htmlFor="" className="text-[20px] rounded-md text-black">
+                  CVV
+                </label>
+                <input
+                  type="number"
+                  className="w-full h-[40px] rounded-md bg-[#D9D9D9] pl-2"
+                />
+              </div>
             </div>
-          </div>
+            <button className="bg-[#daa520] hover:bg-[#bf2034] text-white font-bold py-2 px-4 rounded cursor-pointer">
+              Finalizar Compra
+            </button>
+          </div>}
         </div>
-       </div>
       </div>
+    </div>
   )
 }
