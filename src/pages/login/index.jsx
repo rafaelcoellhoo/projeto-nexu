4a62580e5ff636624716bf11c41d43aac974a74b
Mikelemag <mikelepinto2017@gmail.com>
2025-05-22 11:46:41 -0300

    finalizando-nexu

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,41 +1,50 @@
-import { ToastContainer } from "react-toastify"
+import { toast, ToastContainer } from "react-toastify"
 import { FcGoogle } from "react-icons/fc"
 import { useState } from "react"
-import teste from "/public/login-page.svg"
-import { FaGithub, FaSquareGithub } from "react-icons/fa6";
+
+
 
 export default function Login(){
-   const [usuario, setUsario] = useState("")
+   const [usuario, setUsuario] = useState("")
    const [senha, setSenha] = useState("")
 
+
    function Login(){
-    if(!user || !pass)
+    if(!usuario || !senha)
       return toast.error("Preencha todos os campos")
- 
-  
-   }
+    if(usuario.length < 8 || senha.length < 8){
+      return toast.error("Usuario ou senha inválidos")
+
+    }
+     
+    console.log(usuario)
+    console.log(senha)
+    return toast.success(" Login realizado com sucesso!")
+
+  }
+
+
+
+
+
 
 
   return (
     <div className="w-full h-screen  bg-[#151c16]">
-      <h1 className="text-2xl text-center bg-[#aadfa8] p-4 font-bold">Faça login e encontre seu parceiro ideal para atividades físicas!</h1>
+      <ToastContainer />
+      <h1 className="text-2xl text-center bg-[#74c57d] p-4 font-bold">Faça login e encontre seu parceiro ideal para atividades físicas!</h1>
       <div className="w-full flex items-center justify-center ">
-        <img src={teste.src} width={600} alt="" />
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          theme="colored"
+        
 
-        />
-
-        <div className="w-[30%] h-full flex items-center justify-center">
+        <div className="w-[30%] h-full flex items-center justify-center mt-40">
           <div className="w-[70%] h-[450px] bg-[#141414] rounded-md">
             <div className="w-full h-[15%] flex items-center justify-center">
-              <h1 className="text-[30px] text-[#74c57d] font-semibold">PÁGINA DE ACESSO!</h1>
+              <h1 className="text-[30px] text-[#74c57d] font-semibold">PÁGINA DE ACESSO</h1>
             </div>
             <div className="w-full h-[60%] p-[20px]">
               <label htmlFor="user" className="text-[#74c57d]">Usuário</label>
               <input
+              onChange={(event)=> {setUsuario(event.target.value)}}
                 placeholder="usuario"
                 type="text"
                 id="user"
@@ -43,6 +52,8 @@ export default function Login(){
               />
               <label htmlFor="pass" className="text-[#74c57d]">Senha</label>
               <input
+              onChange={(event)=> {setSenha(event.target.value)}}
+              
                 placeholder="senha"
 
 
@@ -50,28 +61,35 @@ export default function Login(){
                 id="pass"
                 className="w-full rounded-md p-[10px] bg-[#d6c7c7] text-[#0e0e0e]"
               />
+              
               <button
+              onClick={Login}
                 className="w-full h-[50px] rounded-lg mt-6 bg-[#87e099] text-[20px]
              font-semibold text-[#201b2c] cursor-pointer"
+             
                 
               >ENTRAR
 
               </button>
+              
               <div className="w-full flex items-center justify-center text-[#74c57d] pt-2">
                 <a href="">Esqueceu sua Senha?</a>
               </div>
             </div>
+            
 
             <div className="w-full h-[25%] p-[20px] flex items-center">
+              
 
-              <button
+              <button 
+              onClick={() => window.location.href = "/"}
                 className="w-full h-[60px] rounded-lg flex flex-col items-center pt-[2px] bg-[#87e099] text-[15px] 
-           font-semibold text-[#201b2c] cursor-pointer"
+           font-bold text-[#201b2c] cursor-pointer"
               >Entrar com Google
-                <FcGoogle size={70} />
-                <FaSquareGithub size={70} />
+                <FcGoogle size={70}/>
                 
               </button>
+              
             </div>
 
           </div>
